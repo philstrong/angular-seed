fecad9fe9a21732d18c74f0eaa6b2c81a8210e41
Phil Strong <phil.strong@sencha.com>
2013-03-06 10:45:18 -0800

    correct

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,15 +10,19 @@ function TodoCtrl($scope) {
         return $scope.todos.length;
     };
 
-    $scope.today = function() {
+    $scope.getToday = function() {
         var start = new Date();
         start.setHours(0,0,0,0);
 
         var end = new Date();
         end.setHours(23,59,59,999);
+
+        var weekout = new Date(end);
+        weekout = new Date(weekout.setDate(end.getDate() + 7));
         return {
             start: start,
-            end: end
+            end: end,
+            weekout: weekout
         };
     };
 
@@ -34,14 +38,14 @@ function TodoCtrl($scope) {
     $scope.late = function() {
         var late = [],
             todos = $scope.todos,
-            today = Date.now(),
+            today = $scope.getToday(),
             dt;
 
 
         for (var i = 0; i < todos.length; i++) {
             dt = new Date(todos[i].due);
 
-            if (dt < today && !$scope.isToday(dt)) {
+            if (dt < today.start) {
                 late.push(todos[i]);
             }
         }
@@ -53,14 +57,14 @@ function TodoCtrl($scope) {
     $scope.today = function() {
         var late = [],
             todos = $scope.todos,
-            today = Date.now(),
-            dt, seconds, interval;
+            today = $scope.getToday(),
+            dt;
 
 
         for (var i = 0; i < todos.length; i++) {
             dt = new Date(todos[i].due);
 
-            if ($scope.isToday(dt)) {
+            if (dt >= today.start && dt <= today.end) {
                 late.push(todos[i]);
             }
         }
@@ -70,14 +74,13 @@ function TodoCtrl($scope) {
     $scope.soon = function() {
         var late = [],
             todos = $scope.todos,
-            today = $scope.today(),
-            weekout = today.end.getDate() + 7,
+            today = $scope.getToday(),
             dt;
 
 
         for (var i = 0; i < todos.length; i++) {
             dt = new Date(todos[i].due);
-            if (dt > today.end && dt < weekout) {
+            if (dt > today.end && dt < today.weekout) {
                 late.push(todos[i]);
             }
         }
@@ -87,14 +90,13 @@ function TodoCtrl($scope) {
     $scope.someday = function() {
         var late = [],
             todos = $scope.todos,
-            today = $scope.today(),
-            weekout = today.end.getDate() + 7,
+            today = $scope.getToday(),
             dt;
 
 
         for (var i = 0; i < todos.length; i++) {
             dt = new Date(todos[i].due);
-            if (dt > weekout) {
+            if (dt > today.weekout) {
                 late.push(todos[i]);
             }
         }
@@ -108,8 +110,8 @@ function TodoCtrl($scope) {
         // today, tommorow
         date = new Date(date);
 
-        seconds = Math.floor((now - date) / 1000);
         if (date < now) {
+            seconds = Math.floor((now - date) / 1000);
             // late
             interval = Math.floor(seconds / 31536000);
 
@@ -126,14 +128,15 @@ function TodoCtrl($scope) {
             }
             interval = Math.floor(seconds / 3600);
             if (interval > 1) {
-                return "today";
+                return interval + " hours ago";
             }
             interval = Math.floor(seconds / 60);
             if (interval > 1) {
-                return "today";
+                return "less than an hour";
             }
             return Math.floor(seconds) + " seconds";
         } else {
+            seconds = Math.floor((date - now) / 1000);
             interval = Math.floor(seconds / 31536000);
             if (interval > 1) {
                 return "in " + interval + " years";
@@ -161,9 +164,78 @@ function TodoCtrl($scope) {
     };
 
     $scope.todos = [
-        {"title":"Pay electric bill","duration":600,"due":"2013-03-01T08:00:00.000Z","created":"2013-03-06T00:57:47.000Z","updated":"2013-03-06T00:57:47.000Z","completed":null,"deleted":false,"id":"b1-68691467-45"},
-        {"title":"Walk","duration":600,"due":"2013-03-06T08:00:00.000Z","created":"2013-03-06T00:57:47.000Z","updated":"2013-03-06T00:57:47.000Z","completed":null,"deleted":false,"id":"b1-68691467-45"},
-        {"title":"Walk","duration":600,"due":"2013-03-05T08:00:00.000Z","created":"2013-03-06T00:57:47.000Z","updated":"2013-03-06T00:57:47.000Z","completed":null,"deleted":false,"id":"b1-68691467-45"}
+        {
+            title: 'Fix my bike',
+            duration: 20,
+            due:"2013-03-01T08:00:00.000Z",
+            created:"2013-03-01T08:00:00.000Z",
+            completed: null,
+            deleted: false
+        },
+        {
+            title: 'Write up meeting notes',
+            duration: 60,
+            due:"2013-04-01T08:00:00.000Z",
+            created:"2013-03-01T08:00:00.000Z",
+            completed: null,
+            deleted: false
+        },
+        {
+            title: 'Floss',
+            duration: 1,
+            due:"2013-01-01T08:00:00.000Z",
+            created:"2013-03-01T08:00:00.000Z",
+            completed: null,
+            deleted: false
+        },
+        {
+            title: 'Meet with stress councillor',
+            duration: 50,
+            due:"2013-01-15T08:00:00.000Z",
+            created:"2013-03-01T08:00:00.000Z",
+            completed: null,
+            deleted: false
+        },
+        {
+            title: 'Bake a cake for Aunty Flo',
+            duration: 35,
+            due:"2013-03-20T08:00:00.000Z",
+            created:"2013-03-01T08:00:00.000Z",
+            completed: null,
+            deleted: false
+        },
+        {
+            title: 'Organize recycling',
+            duration: 41,
+            due:"2013-06-20T08:00:00.000Z",
+            created:"2013-03-01T08:00:00.000Z",
+            completed: null,
+            deleted: false
+        },
+        {
+            title: 'Call locksmith',
+            duration: 3,
+            due:"2013-03-06T13:00:00.000Z",
+            created:"2013-03-01T08:00:00.000Z",
+            completed: null,
+            deleted: false
+        },
+        {
+            title: 'Book haircut',
+            duration: 3,
+            due:"2013-03-06T08:00:00.000Z",
+            created:"2013-03-01T08:00:00.000Z",
+            completed: null,
+            deleted: false
+        },
+        {
+            title: 'Test test test',
+            duration: 3,
+            due:"2013-03-05T08:00:00.000Z",
+            created:"2013-03-01T08:00:00.000Z",
+            completed: "2013-03-05T08:00:00.000Z",
+            deleted: true
+        }
     ];
 }
 // TodoItemCtrl.$inject = [];
